4eb317851998961b0c20579c850154af41921f3d
E-Rick-M <erickmuirurinjuguna@gmail.com>
2025-07-09 21:04:06 +0300

    Added using OpenAi sdk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 import json
-def generate_x_post(topic:str ,history:list)-> str:
+from openai import OpenAI
+
+client= OpenAI(base_url="http://localhost:11434/v1", api_key="ollama")
+
+def generate_x_post_usingOpenAI(topic:str ,history:list):
     prompt=f"""You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter). Your task is to create a post that will capture the attention of users and encourage them to interact with it.
     Your task is to generate a post that is concise, engaging, and relevant to the topic at hand. The post should be no longer than 280 characters and tailored to the topic provided by the user.
     Avoid using hashtags, emojis, or any other special characters. The post should be written in a professional tone and should not include any personal opinions or biases.
@@ -13,35 +17,61 @@ def generate_x_post(topic:str ,history:list)-> str:
     """
    
     history.append({"role": "system", "content": prompt})
-    response=requests.post("http://localhost:11434/api/chat",json={
-        "model": "qwen3:4b",
-        "messages": history,
-        "stream":True,
-    })
-
-    if response.status_code != 200:
-        print(f"Error: {response.status_code} - {response.text}")
-        return None
-    print("Response from the server:")
-    content = ""
-    for line in response.iter_lines():
-        if line:
-            try:
-                data =json.loads(line)
-                if data.get("done"):
-                    break
-                if "message" in data and data["message"].get("role") == "assistant":
-                    chunk = data["message"].get("content", "")
-                    content += chunk
-                    print(chunk, end="", flush=True)
-            except json.JSONDecodeError as e:
-                print(f"Error processing line: {e}-Line: {line}")
-                continue
-    if not content:
-        print("No content generated."+ "-" * 50)
-        return None
+    
+    response=client.chat.completions.create(
+        # model="qwen3:4b",
+        model="gemma3:4b",
+        messages=history,
+        stream=False,
+    )
+   
     print("\nPost generation complete.")
-    return content
+    return response.choices[0].message.content
+ 
+
+
+# def generate_x_post(topic:str ,history:list)-> str:
+#     prompt=f"""You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter). Your task is to create a post that will capture the attention of users and encourage them to interact with it.
+#     Your task is to generate a post that is concise, engaging, and relevant to the topic at hand. The post should be no longer than 280 characters and tailored to the topic provided by the user.
+#     Avoid using hashtags, emojis, or any other special characters. The post should be written in a professional tone and should not include any personal opinions or biases.
+    
+#     Here's the topic provided by the user for which you need to generate a post:
+    
+#     <topic>
+#     {topic}
+#     </topic>
+#     """
+   
+#     history.append({"role": "system", "content": prompt})
+#     response=requests.post("http://localhost:11434/api/chat",json={
+#         "model": "qwen3:4b",
+#         "messages": history,
+#         "stream":True,
+#     })
+
+#     if response.status_code != 200:
+#         print(f"Error: {response.status_code} - {response.text}")
+#         return None
+#     print("Response from the server:")
+#     content = ""
+#     for line in response.iter_lines():
+#         if line:
+#             try:
+#                 data =json.loads(line)
+#                 if data.get("done"):
+#                     break
+#                 if "message" in data and data["message"].get("role") == "assistant":
+#                     chunk = data["message"].get("content", "")
+#                     content += chunk
+#                     print(chunk, end="", flush=True)
+#             except json.JSONDecodeError as e:
+#                 print(f"Error processing line: {e}-Line: {line}")
+#                 continue
+#     if not content:
+#         print("No content generated."+ "-" * 50)
+#         return None
+#     print("\nPost generation complete.")
+#     return content
 
 def main():
     history = []
@@ -52,7 +82,8 @@ def main():
     print("**" * 50)
     input_topic = input("Enter the topic for the X post: ")
     history.append({"role": "user", "content": input_topic})
-    post = generate_x_post(input_topic , history)
+    # post = generate_x_post(input_topic , history)
+    post=generate_x_post_usingOpenAI(input_topic, history)
 
     print("-" * 50)
     print("Here is your generated X post:")
